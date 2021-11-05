1a2851f20b2e42c2ceacb7b74e63fa3af590e0f6
maromaSamsa <y9740201@gmail.com>
2021-11-05 10:37:20 +0800

    Initial Commit

---
+++ Dual_camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dual_camera
+//
+//  Created by WTMH on 2021/11/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dual_camera/Dual_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dual_cameraApp.swift
+//  Dual_camera
+//
+//  Created by WTMH on 2021/11/5.
+//
+
+import SwiftUI
+
+@main
+struct Dual_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
